df341cc43215485b91be9f148bb4c347ba6ed024
Paul Kmiec <paul.kmiec@appfolio.com>
2015-09-02 00:46:06 -0700

    added ability to rerun the last run / run focused test

--- Support/RubyMate/run_script.rb
+++ Support/RubyMate/run_script.rb
@@ -1,74 +1,10 @@
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/tm/executor"
 require "#{ENV["TM_SUPPORT_PATH"]}/lib/tm/save_current_document"
 require "pathname"
+require 'fileutils'
+require "yaml"
 
 TextMate.save_if_untitled
-
-# For Run focused unit test, find the name of the test the user wishes to run.
-args = [ ]
-if ARGV.first == "--name="
-  n = ENV['TM_LINE_NUMBER'].to_i
-
-  should, spec, context, name, test_name = nil, nil, nil
-
-  File.open(ENV['TM_FILEPATH']) do |f|
-    # test/unit
-    lines     = f.read.split("\n")[0...n].reverse
-    name      = lines.find { |line| line =~ /^\s*def test[_a-z0-9]*[\?!]?/i }.to_s.sub(/^\s*def (.*?)\s*$/) { $1 }
-    # test helper
-    test_name = $2 || $3 if lines.find { |line| line =~ /^\s*test\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
-    # test/spec.
-    spec      = $3 || $4 if lines.find { |line| line =~ /^\s*(specify|it)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
-    context   = $3 || $4 if lines.find { |line| line =~ /^\s*(context|describe)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
-    # shoulda.
-    should    = $3 || $4 if lines.find { |line| line =~ /^\s*(should)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
-    context   = $3 || $4 if lines.find { |line| line =~ /^\s*(context)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
-  end
-  
-  if name and !name.empty?
-    args << "--name=#{name}"
-  elsif test_name and !test_name.empty?
-    args << "--name=test_#{test_name.gsub(/\s+/,'_')}"
-  elsif spec and !spec.empty? and context and !context.empty?
-    args << %Q{--name="/test_spec \\{.*#{context}\\} \\d{3} \\[#{spec}\\]/"}
-  elsif should and !should.empty? and context and !context.empty?
-    test_name = "#{context} should #{should}".gsub(/[\$\?\+\.\s\'\"\(\)]/,'.?')
-    args << "--name=/#{test_name}/"
-  else
-    puts "Error:  This doesn't appear to be a TestCase or spec."
-    exit
-  end
-end
-
-is_test_script = !(ENV["TM_FILEPATH"].match(/(?:\b|_)(?:tc|ts|test)(?:\b|_)/).nil? and
-  File.read(ENV["TM_FILEPATH"]).match(/\brequire\b.+(?:test\/unit|test_helper)/).nil?)
-
-cmd = [ENV['TM_RUBY'] || 'ruby', '-rcatch_exception']
-test_root = nil
-
-if is_test_script and not ENV['TM_FILE_IS_UNTITLED']
-  path_ary = ENV['TM_FILEPATH'].split("/")
-  if index = path_ary.rindex("test")
-    test_root  = File.expand_path(File.join(*(path_ary[0..-2] + [".."] * (path_ary.length - index - 1))))
-    # test_path = "#{File.join(*path_ary[0..index])}:#{File.join(*path_ary[0..-2])}"
-    # lib_path  = File.join(test_root, "lib")
-    
-    spring_path = File.join(test_root, "bin", "testunit")
-    if File.exist?(spring_path)
-      cmd << spring_path
-    else
-      cmd << "-Itest"
-      # if File.exist? lib_path
-      #   cmd << "-I#{lib_path}:#{test_path}"
-      # else
-      #   cmd << "-I#{test_path}"
-      # end
-    end
-    
-  end
-end
-
-cmd << ENV["TM_FILEPATH"]
 
 def path_to_url_chunk(path)
   unless path == "untitled"
@@ -103,12 +39,110 @@ def actual_path_name(path)
   return path, '', path
 end
 
+rerun_root = File.join(ENV['TMPDIR'], 'rerun')
+rerun_path = File.join(rerun_root, "#{ENV['TM_PROJECT_UUID']}.yml")
+FileUtils.mkdir_p(rerun_root)
+
+args           = []
+use_hashbang   = !ENV.has_key?('TM_RUBY')
+cmd            = [ENV['TM_RUBY'] || 'ruby', '-rcatch_exception']
+test_root      = nil
+is_test_script = !(ENV["TM_FILEPATH"].match(/(?:\b|_)(?:tc|ts|test)(?:\b|_)/).nil? and
+  File.read(ENV["TM_FILEPATH"]).match(/\brequire\b.+(?:test\/unit|test_helper)/).nil?)
+verb           = "Running"
+noun           = ENV['TM_DISPLAYNAME']
+
+if ARGV.first == "--rerun" && File.exist?(rerun_path)
+  rerun_options = YAML.load(File.read(rerun_path))
+
+  cmd            = rerun_options['cmd']
+  args           = rerun_options['args']
+  use_hashbang   = rerun_options['use_hashbang']
+  test_root      = rerun_options['test_root']
+  is_test_script = rerun_options['is_test_script']
+  noun           = rerun_options['noun']
+  verb           = "Re-running"
+  
+else
+  # For Run focused unit test, find the name of the test the user wishes to run.
+  if ARGV.first == "--name="
+    n = ENV['TM_LINE_NUMBER'].to_i
+
+    should, spec, context, name, test_name = nil, nil, nil
+
+    File.open(ENV['TM_FILEPATH']) do |f|
+      # test/unit
+      lines     = f.read.split("\n")[0...n].reverse
+      name      = lines.find { |line| line =~ /^\s*def test[_a-z0-9]*[\?!]?/i }.to_s.sub(/^\s*def (.*?)\s*$/) { $1 }
+      # test helper
+      test_name = $2 || $3 if lines.find { |line| line =~ /^\s*test\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
+      # test/spec.
+      spec      = $3 || $4 if lines.find { |line| line =~ /^\s*(specify|it)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
+      context   = $3 || $4 if lines.find { |line| line =~ /^\s*(context|describe)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
+      # shoulda.
+      should    = $3 || $4 if lines.find { |line| line =~ /^\s*(should)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
+      context   = $3 || $4 if lines.find { |line| line =~ /^\s*(context)\s+('(.*)'|"(.*)")+\s*(\{|do)/ }
+    end
+  
+    if name and !name.empty?
+      args << "--name=#{name}"
+    elsif test_name and !test_name.empty?
+      args << "--name=test_#{test_name.gsub(/\s+/,'_')}"
+    elsif spec and !spec.empty? and context and !context.empty?
+      args << %Q{--name="/test_spec \\{.*#{context}\\} \\d{3} \\[#{spec}\\]/"}
+    elsif should and !should.empty? and context and !context.empty?
+      test_name = "#{context} should #{should}".gsub(/[\$\?\+\.\s\'\"\(\)]/,'.?')
+      args << "--name=/#{test_name}/"
+    else
+      puts "Error:  This doesn't appear to be a TestCase or spec."
+      exit
+    end
+  end
+
+  if is_test_script and not ENV['TM_FILE_IS_UNTITLED']
+    path_ary = ENV['TM_FILEPATH'].split("/")
+    if index = path_ary.rindex("test")
+      test_root  = File.expand_path(File.join(*(path_ary[0..-2] + [".."] * (path_ary.length - index - 1))))
+      # test_path = "#{File.join(*path_ary[0..index])}:#{File.join(*path_ary[0..-2])}"
+      # lib_path  = File.join(test_root, "lib")
+    
+      spring_path = File.join(test_root, "bin", "testunit")
+      if File.exist?(spring_path)
+        cmd << spring_path
+      else
+        cmd << "-Itest"
+        # if File.exist? lib_path
+        #   cmd << "-I#{lib_path}:#{test_path}"
+        # else
+        #   cmd << "-I#{test_path}"
+        # end
+      end
+    
+    end
+  end
+
+  cmd << ENV["TM_FILEPATH"]
+
+  rerun_options = {
+    'is_test_script' => is_test_script,
+    'test_root'      => test_root,
+    'cmd'            => cmd,
+    'args'           => args,
+    'use_hashbang'   => use_hashbang,
+    'noun'           => noun,
+  }
+
+  File.open(rerun_path, 'w+') { |out| YAML.dump(rerun_options, out ) }
+end
+
 Dir.chdir(test_root) if test_root # in order for ruby version to be correct
 TextMate::Executor.run( cmd, :chdir => test_root,
                              :version_args => ["--version"],
-                             :use_hashbang => !ENV.has_key?('TM_RUBY'),
+                             :use_hashbang => use_hashbang,
                              :create_error_pipe => true,
-                             :script_args  => args ) do |line, type|
+                             :script_args  => args,
+                             :verb => verb,
+                             :noun => noun ) do |line, type|
   #  
   if is_test_script and type == :out
     
